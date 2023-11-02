35b6755da5ad90a6caf77ba29e00535b5f3a3940
PuruMukherjee <61185540+PuruMukherjee@users.noreply.github.com>
2023-11-02 19:54:52 +0530

    added year and month

--- prices.py
+++ prices.py
@@ -2,3 +2,6 @@ name = "Python"
 print(f"{name} is fun")
 version_control = "git"
 print(f'Using {version_control} for SVC')
+year  = 2023
+month  = "November"
+print(f'Writing this in {month}, {year}')
